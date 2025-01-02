66d5ce04d9b502facd682199ba5d4e87653095b7
NorthHuang <hzl136133@gmail.com>
2025-01-02 21:22:31 +1100

    debug

--- generate_config.rb
+++ generate_config.rb
@@ -5,6 +5,6 @@ config = File.exist?(config_file) ? YAML.load_file(config_file) : {}
 
 config['formspree_form_path'] = ENV['FORMSPREE_FORM_PATH']
 config['google_recaptcha_site_key'] = ENV['GOOGLE_RECAPTCHA_SITE_KEY']
-puts "Formspree path: #{ENV['FORMSPREE_FORM_PATH']}"
+config['debug'] = ENV['DEBUG']
 File.open(config_file, 'w') { |file| file.write(config.to_yaml) }
 puts "Updated _config.yml with Formspree Form PATH and Google ReCAPTCHA Site Key"
